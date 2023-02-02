e011b8eb7f336e2b7d749e44cf584c5f3b7052a0
Surya <suryasekhardaripa60461@gmail.com>
2023-02-02 13:42:37 +0530

    done

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -3,10 +3,17 @@ import java.util.Arrays;
 
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        
+        int[][] result = new int[matrix1.length][matrix2[0].length];
 
-        // Put your code here
-
-        return null;
+        for(int i =0;i<matrix1.length;i++){
+            for(int j=0;j<matrix2[0].length;j++){
+                for(int k=0;k<matrix2.length;k++){
+                    result[i][j] += matrix1[i][k]*matrix2[k][j];
+                }
+            }      
+        }
+        return result;
     }
 
     public static void main(String[] args) {
